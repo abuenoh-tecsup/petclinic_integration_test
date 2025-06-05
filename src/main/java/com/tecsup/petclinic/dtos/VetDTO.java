837ff9bfd589f5b52414510df1b56a33f8de427f
Vania <sonalysifuentes@gmail.com>
2025-06-05 13:36:00 -0500

    VetPDTO created

--- src/main/java/com/tecsup/petclinic/dtos/VetDTO.java
+++ src/main/java/com/tecsup/petclinic/dtos/VetDTO.java
@@ -1,4 +1,17 @@
 package com.tecsup.petclinic.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class VetDTO {
+
+    private Integer id;
+
+    private String firstname;
+
+    private String lastname;
 }
