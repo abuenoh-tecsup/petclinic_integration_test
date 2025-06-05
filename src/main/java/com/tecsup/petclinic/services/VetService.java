a0ab4cf772d8226193008678ddf7a7dcd3485d06
Vania <sonalysifuentes@gmail.com>
2025-06-05 13:29:51 -0500

    updates

--- src/main/java/com/tecsup/petclinic/services/VetService.java
+++ src/main/java/com/tecsup/petclinic/services/VetService.java
@@ -1,7 +1,7 @@
 package com.tecsup.petclinic.services;
 
 import com.tecsup.petclinic.entities.Vet;
-import com.tecsup.petclinic.exceptions.VetNotFoundException;
+import com.tecsup.petclinic.exception.VetNotFoundException;
 
 import java.util.List;
 
--- src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
@@ -1,7 +1,7 @@
 package com.tecsup.petclinic.services;
 
 import com.tecsup.petclinic.entities.Vet;
-import com.tecsup.petclinic.exceptions.VetNotFoundException;
+import com.tecsup.petclinic.exception.VetNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
